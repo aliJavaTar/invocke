2c0977bfa040dfbd6898b188100f67bf4cc00ba3
aliJavaTar <ali77erfagh99@gmail.com>
2022-12-23 11:23:50 +0330

    add rabbitmq config

--- src/main/java/com/google/invocke/infrastructure/RabbitMQConfiguration.java
+++ src/main/java/com/google/invocke/infrastructure/RabbitMQConfiguration.java
@@ -1,8 +1,37 @@
 package com.google.invocke.infrastructure;
 
+import com.rabbitmq.client.impl.AMQImpl;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfiguration {
+    @Value("google.queue")
+    private String queueName;
 
+    @Value("google.exchange")
+    private String exchange;
+
+    @Value("google.routingkey")
+    private String routingKey;
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    DirectExchange exchange() {
+        return new DirectExchange(exchange);
+    }
+
+    @Bean
+    Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+    }
 }
