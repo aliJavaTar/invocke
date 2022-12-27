9a373beb03567a8c7d68739060661dd1446f792f
aliJavaTar <ali77erfagh99@gmail.com>
2022-12-27 11:55:27 +0330

    add doc to controller

--- src/main/java/com/google/invocke/presentation/InvoiceController.java
+++ src/main/java/com/google/invocke/presentation/InvoiceController.java
@@ -5,6 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.invocke.application.PrintInvoice;
 import com.google.invocke.domin.Invoice;
 import com.google.invocke.infrastructure.entity.InvoiceEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import org.springframework.validation.annotation.Validated;
@@ -16,6 +20,12 @@ import org.springframework.web.bind.annotation.*;
 public class InvoiceController {
     private final PrintInvoice printInvoice;
 
+    @Operation(summary = "this is first test of swagger")
+    @ApiResponses(value =
+            {
+                    @ApiResponse(responseCode = "200", description = "ok"),
+                    @ApiResponse(responseCode = "404", description = "not found",content = @Content)
+            })
     @PostMapping
     public void saveInvoice(@Validated @RequestBody InvoiceEntity invoice) {
         printInvoice.sendMessage(invoice);
